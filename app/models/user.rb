1a236fcb7a4d1f4dc8c001aa7b0357c428838525
acordova <ae_cordova@outlook.com>
2020-03-26 15:08:22 -0600

    Added EventAttendances model and associations for attendees and attended events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :created_events, class_name: 'Event', foreign_key: 'organizer_id'
+  has_many :event_attendances
+  has_many :attending_events, through: 'event_attendances', source: 'event'
 end
