fc746419b7de1c1f3ecf0dc48a65291fa0297a4e
Rey David Cuevas <cuevas.vela@gmail.com>
2020-03-27 10:58:21 -0600

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :created_events, class_name: 'Event', foreign_key: 'organizer_id'
   has_many :event_attendances
   has_many :attending_events, through: 'event_attendances', source: 'event'
+
+  validates :name, presence: true, length: { maximum: 100 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 end
