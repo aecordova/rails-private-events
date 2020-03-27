269c824b9969a51a56fe4a43040391427df85646
Rey David Cuevas <cuevas.vela@gmail.com>
2020-03-27 11:02:20 -0600

    added validation to events model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,11 @@ class Event < ApplicationRecord
   belongs_to :organizer, class_name: 'User'
   has_many :event_attendances
   has_many :attendees, through: 'event_attendances', source: 'user'
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
+  validates :date, presence: true
+  validates :place, presence: true , lenght: { maximum: 150 }
+  validates :organizer_id, presence: true
 
   scope :future, -> {where("date >= ?", DateTime.now )}
   scope :past, -> {where("date < ?", DateTime.now )}
