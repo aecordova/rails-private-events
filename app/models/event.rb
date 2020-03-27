2211b23c1b75a03f4f7b0eba9f0b18aaa7be9efc
acordova <ae_cordova@outlook.com>
2020-03-27 10:54:54 -0600

    Added Upcoming and Past Event breakdown

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :organizer, class_name: 'User'
   has_many :event_attendances
   has_many :attendees, through: 'event_attendances', source: 'user'
+
+  scope :future, -> {where("date >= ?", DateTime.now )}
+  scope :past, -> {where("date < ?", DateTime.now )}
 end
